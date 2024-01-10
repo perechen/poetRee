5062cdd92fcc9f9615668973fa20475de2d9dc5d
Artjom <artemy.she@gmail.com>
2024-01-10 17:32:42 +0100

    finalizing get_poems() methods

--- R/get_poems.R
+++ R/get_poems.R
@@ -1,12 +1,65 @@
-get_poems <- function(corpus="cs") {
+#' List PoeTree poems (metadata only)
+#'
+#' @param corpus string, ISO code of the corpus language. Check get_metadata() for all available corpora.
+#' @param authors integer or vector of integers, author ID. Check get_authors() for these. Supports multiple ids provided in a vector.
+#' @param duplicates logical. Whether to include duplicated texts. Default is FALSE. See more about duplicate detection strategy: https://versologie.cz/poetree/deduplication/
+#' @param tidy logical. If TRUE it unnests poem records that have multiple authors (one poem-author per row format). If FALSE, it bundles author IDs in a string (one poem per row format). Very rarely useful, TRUE by default
+#' @param list_all logical. If TRUE, lists all poems available in the corpus. Default is FALSE.
+#'
+#' @return returns tidy data frame (tibble)
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#'df <- get_poems(corpus="cs",author=c(81,52))
+#'}
+
+get_poems <- function(corpus="cs",
+                      authors=NULL,
+                      duplicates=FALSE,
+                      tidy=TRUE,
+                      list_all=FALSE) {
+
+  if(is.null(authors) & !list_all)
+  {
+    stop("Please provide author IDs, or use `list_all=TRUE`")
+  }
 
   ## list all author ids per corpus
+  if(list_all) {
   df_aut <- get_authors(corpus=corpus)
   authors_v <- df_aut$id_
+  } else {
+
+    authors_v <- authors
+
+  }
 
   ## get list of poems for each author
-  df <- lapply(authors_v, get_poems_a,corpus=corpus) %>%
+  df <- lapply(authors_v, get_poems_a,corpus=corpus,duplicates=duplicates) %>%
     bind_rows()
+
+  ## handling multiple authors per poem if tidy=FALSE
+  if(!tidy) {
+
+    poems <- unique(df$id_) %>% length()
+    rows <- nrow(df)
+
+    # only execute if n unique poems < n of rows in the dataframe
+    ## return original if there are no multiple authors per poem
+
+
+    if(poems < rows) {
+
+    df <- df %>%
+        group_by(across(c(-.data$id_author))) %>%
+        summarise(id_author=paste0(.data$id_author, collapse=",")) %>%
+        ungroup()
+
+    }
+
+
+  }
 
 
   return(df)
--- R/get_poems_a.R
+++ R/get_poems_a.R
@@ -1,9 +1,23 @@
+#' Get poem metadata by author ID
+#'
+#' @param corpus string, ISO code of the corpus language. Check get_metadata() for all available corpora.
+#' @param author integer, author ID. Check get_authors() for these.
+#' @param duplicates logical. Whether to include duplicated texts. Default is FALSE. See more about duplicate detection strategy: https://versologie.cz/poetree/deduplication/
+#'
+#' @return returns tidy data frame (tibble)
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#'df <- get_poems_a(corpus="cs",author=81)
+#'}
 get_poems_a <- function(corpus,
-                        author) {
+                        author,
+                        duplicates=FALSE) {
 
-  author = paste0("&id_author=",author)
+  author_req = paste0("&id_author=",author)
   # request
-  base_url <- paste0("https://versologie.cz/poetree/api/poems?corpus=",corpus,author)
+  base_url <- paste0("https://versologie.cz/poetree/api/poems?corpus=",corpus,author_req)
 
   # handling json output
   out <- GET(base_url)
@@ -12,13 +26,16 @@ get_poems_a <- function(corpus,
   # converting to tibble
   # additional handling of column types
   df <- fromJSON(json) %>%
-    as_tibble() #%>%
-#    mutate(across(c(starts_with("year"),"id_source"),~as.integer(.x)),
-#           across("duplicate", ~as.character(.x)))
+    as_tibble() %>%
+    mutate(across(c(starts_with("year"),"id_source"),~as.integer(.x)),
+           across("duplicate", ~as.character(.x)),
+           id_author=as.integer(author))
+
+  if(!duplicates) {
+
+    df <- df %>% filter(.data$duplicate == "FALSE")
+
+  }
 
   return(df)
 }
-#
-# ,
-# .data$duplicate=as.character(.data$duplicate),
-# .data$title=as.character(.data$title)) %>%
